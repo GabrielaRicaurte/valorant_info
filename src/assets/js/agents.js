b507ed18fed77f7b804144b5051f798e3b2e8dd5
GabrielaRicaurte <gabrielaricaurte0109@gmail.com>
2024-02-22 16:55:09 -0400

    commit agents

--- src/assets/js/agents.js
+++ src/assets/js/agents.js
@@ -19,9 +19,9 @@ function viewData(datos) {
     background,
     abilities,
     role,
-  } = datos
+  } = datos;
 
-  div.classList.remove("justify-center", "items-center");
+  div.classList.remove("justify-center");
   div.innerHTML = `
         <div class="w-6/12">
             <div style="background-image: url('${background}')" class="h-[80vh] bg-contain bg-no-repeat bg-center">
@@ -31,17 +31,20 @@ function viewData(datos) {
         <div class="w-6/12 py-1">
           <h1 class="font-oswald text-4xl uppercase text-center font-bold">${displayName}</h1>
           <p class="font-bold mt-2 px-5">${description}</p>
-          <div id="abilities">
+          <p class="font-bold mt-2 px-5">Role: ${role.description}</p>
+          <div id="abilities" class="px-5 mt-5 flex justify-center gap-x-4">
           </div>
         </div>
     `;
 
-    const abilitiesDiv = document.getElementById("abilities");
-    abilities.forEach(ability => {
-      const { displayName } = ability
-      abilitiesDiv.innerHTML = `
-        <p class="text-center font-bold">${displayName}</p>
+  const abilitiesDiv = document.getElementById("abilities");
+  for(let i = 0; i < abilities.length; i++) {
+    console.log(abilities[i].displayName);
+    abilitiesDiv.innerHTML += `
+      <div class="flex flex-col justify-center items-center">
+        <p class="font-bold">${abilities[i].displayName}</p>
+        <img src="${abilities[i].displayIcon}" alt="img" class="w-10 h-10">
+      </div>
       `
-  });
-    
+  }
 }
